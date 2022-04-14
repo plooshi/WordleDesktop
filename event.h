847e1b711e1d6c312e26834967f939af363aa288
guacaplushy <guacaplushy@icloud.com>
2022-04-13 21:14:58 -0400

    include guards yay

--- event.h
+++ event.h
@@ -1,4 +1,8 @@
+#pragma once
+#ifndef EVENT_INCL
+#define EVENT_INCL 1
 #include "gui.h"
 #include "common.h"
 
 gboolean on_key_press (GtkWidget *widget, GdkEventKey *event, gpointer user_data);
+#endif
--- gui.h
+++ gui.h
@@ -1,6 +1,10 @@
+#pragma once
+#ifndef GUI_INCL
+#define GUI_INCL 1
 #include <gtk-3.0/gtk/gtk.h>
 #include <gtk-3.0/gdk/gdkkeysyms.h>
 
 static void render(GtkApplication *app, gpointer data);
 
 int gtkMain();
+#endif
